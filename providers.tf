daf3255de6f6910ce55e4be9aba22fb086cbb7cd
Pradeep Reddy B <66411998+Pradeep-VJ@users.noreply.github.com>
2024-12-04 23:59:04 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -17,8 +17,8 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.aws_access_key       # Referenced from Jenkins credentials
-  secret_key = var.aws_secret_key       # Referenced from Jenkins credentials
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 variable "aws_access_key" {
